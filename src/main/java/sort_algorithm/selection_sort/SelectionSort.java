5e25f624da707354ced06078dd437888cf36f5bf
psw4887 <psw4887@naver.com>
2022-08-18 21:49:15 +0900

    Docs: 자바독 및 주석 추가 #2

--- src/main/java/sort_algorithm/selection_sort/SelectionSort.java
+++ src/main/java/sort_algorithm/selection_sort/SelectionSort.java
@@ -6,6 +6,14 @@ import static utils.RandomNumberExtract.numberExtract;
 
 /**
  * 선택 정렬 알고리즘
+ * 선택 정렬 알고리즘은 제자리 정렬 알고리즘의 하나
+ * 1) 주어진 리스트 중 최소값 탐색
+ * 2) 그 값을 배열의 가장 앞에있는 값의 위치와 교체
+ * 3) 이 후 같은 방법으로 정렬
+ *
+ * 시간복잡도: O(n²)
+ *
+ * 알고리즘이 단순하며 사용할 수 있는 메모리가 제한적인 경우 사용 시 성능상의 이점 존재
  *
  * @author 박세완
  */
@@ -24,9 +32,9 @@ public class SelectionSort {
         List<Integer> numberList = numberExtract(MIN, MAX, COLLECTION_SIZE);
         System.out.println("선택 정렬 전: " + numberList);
 
-        for(int i = 0; i < COLLECTION_SIZE - 1; i++) {
-            for(int j = i + 1; j < COLLECTION_SIZE; j++) {
-                selectionSort(numberList, i, j);
+        for(int i = 0; i < COLLECTION_SIZE - 1; i++) { // 마지막 값은 정렬을 필요로 하지 않기에 -1 (기준 값의 위치 값 표시)
+            for(int j = i + 1; j < COLLECTION_SIZE; j++) { // 정렬할 숫자의 개수를 표시 (기준 값 제외한 값들로 비교)
+                selectionSort(numberList, i, j); // 두 수를 검증 (비교)
             }
         }
         System.out.println("선택 정렬 후: " + numberList);
@@ -35,9 +43,9 @@ public class SelectionSort {
     private static void selectionSort(List<Integer> numberList, int i, int j) {
         int temp;
 
-        if(numberList.get(i) > numberList.get(j)) {
+        if(numberList.get(i) > numberList.get(j)) { // 비교를 진행하던 기준값이 비교값보다 큰 경우
             temp = numberList.get(i);
-            numberList.set(i, numberList.get(j));
+            numberList.set(i, numberList.get(j)); // 위치 교체
             numberList.set(j, temp);
         }
     }
