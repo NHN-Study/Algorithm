c7075477b9c5353ea15c2a61be1b10e9eedfdb26
psw4887 <psw4887@naver.com>
2022-08-21 00:43:25 +0900

    Feat: Util 인접리스트 생성 기능 추가 및 JavaDoc 작성 #10

---
+++ src/main/java/search_algorithm/breadth_first_search/BreadthFirstSearch.java
@@ -0,0 +1,20 @@
+package search_algorithm.breadth_first_search;
+
+/**
+ * 너비 우선 탐색 (BFS)
+ * 너비 우선 탐색의 탐색 방식은 다음과 같다.
+ * 1) 시작 정점을 지난 이후 깊이가 1인 모든 정점을 방문
+ * 2) 1인 모든 정점 방문을 마쳤다면, 깊이가 2인 모든 정점 방문
+ * 3) 이런식으로 한 단계씩 깊이를 더해가며 해당 깊이에 있는 모든 정점들을 방문
+ * 4) 더 이상 깊이가 없어 방문할 곳이 없을 시 탐색 종료
+ *
+ *
+ * @author 박세완
+ */
+public class BreadthFirstSearch {
+
+    public static void main(String[] args) {
+
+    }
+
+}
